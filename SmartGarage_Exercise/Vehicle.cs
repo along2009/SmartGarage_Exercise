d02d745c06f663f0604963ace9ac431567ec2bd0
Alon <aganot2009@gmail.com>
2025-10-27 17:04:32 +0200

    תרגיל הגשה

---
+++ SmartGarage_Exercise/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGarage_Exercise
+{
+    internal abstract class Vehicle
+    {
+
+
+        public abstract void moveLeft();
+        public abstract void moveRight();
+        public abstract void refuel();
+    }
+}
